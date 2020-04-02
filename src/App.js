00f69a5e202642c1344643efb21c4f5613efdb0f
Marcin <marcinszuscik11@gmail.com>
2020-04-02 16:44:25 +0200

    Context logic moved to separeted component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { Wrapper } from './Wrapper';
-import { MessagesContext } from './MessagesContext';
+import { MessagesProvider } from './MessagesContext';
 
 class App extends React.Component {
-  handleSubmit = (name, message) => {
-    if (message) {
-      const messages = [...this.state.messages];
-
-      messages.push({ user: name, message });
-      this.setState({ messages });
-    }
-  };
-
-  state = {
-    messages: [],
-    handleSubmit: this.handleSubmit,
-  };
 
   render() {
     return (
       <div className="messenger">
-        <MessagesContext.Provider value={this.state}>
-          <Wrapper onSubmit={this.handleSubmit} />
-        </MessagesContext.Provider>
+        <MessagesProvider>
+          <Wrapper />
+        </MessagesProvider>
       </div>
     );
   }
--- src/MessagesContext.js
+++ src/MessagesContext.js
@@ -4,3 +4,29 @@ export const MessagesContext = React.createContext({
   messages: [],
   handleSubmit: () => {},
 });
+
+export class MessagesProvider extends React.Component {
+  handleSubmit = (name, message) => {
+    if (message) {
+      const messages = [...this.state.messages];
+
+      messages.push({ user: name, message });
+      this.setState({ messages });
+    }
+  };
+
+  state = {
+    messages: [],
+    handleSubmit: this.handleSubmit,
+  };
+
+  render() {
+    return (
+      <div className="messenger">
+        <MessagesContext.Provider value={this.state}>
+          {this.props.children}
+        </MessagesContext.Provider>
+      </div>
+    );
+  }
+}
